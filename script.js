9e4c6983408800d2d755631520059a8056ca77f0
javitron74 <josejaviertroncoso@gmail.com>
2026-08-03 12:40:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -143,7 +143,7 @@ function render() {
 							? `<span class="pill-emision no">${emision}</span> `
 							: `<span class="pill-emision">${emision}</span> `
                         //: "<em>Sin fecha</em>"
-						: "<em>"${hoy}"</em>"
+						: "<em>"${emision}"</em>"
                 }
                 <br><br>
                 ${comentarios || "<em>Sin comentarios</em>"}
