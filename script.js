80dbca5c8b94b645f75a88103115ea23046b7ecd
javitron74 <josejaviertroncoso@gmail.com>
2026-08-06 08:05:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -122,7 +122,7 @@ function render() {
         const hoy = new Date();
         const noEmitido = fechaEmision && fechaEmision > hoy;
 */		
-		const fechaEmision = parseFecha(emision);      
+		const fechaEmision = emision;      
         const hoy = new Date();
 		const fechaHoy = new Date(`${hoy.getFullYear()}-${String(hoy.getMonth() + 1).padStart(2, '0')}-${String(hoy.getDate()).padStart(2, '0')}`);
         const noEmitido = fechaEmision && fechaEmision > fechaHoy;
