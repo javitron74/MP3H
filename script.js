0067bddad5ec00156579e3d3003fe379e5d035d8
javitron74 <josejaviertroncoso@gmail.com>
2026-09-11 13:40:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -175,15 +175,16 @@ function render() {
 		}
         card.innerHTML = `
             <div class="card-header">
-				<div class="edit-icon" onclick="location.href='form-edit.html?pos=${item.Pos}'">
-					<svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
-						<path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04a1.003 1.003 0 0 0 0-1.42l-2.34-2.34a1.003 1.003 0 0 0-1.42 0l-1.83 1.83 3.75 3.75 1.84-1.82z"/>
-					</svg>
-				</div>
                 <div>
                     <div class="card-title">#${item.Pos} · ${item.Banda}</div>
                     <div class="card-subtitle">${item.Disco}</div>
                 </div>
+				<!-- ICONO EDITAR -->
+				<div class="edit-btn" onclick="location.href='form-edit.html?pos=${item.Pos}'">
+					<svg viewBox="0 0 24 24" class="edit-icon">
+						<path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04a1.003 1.003 0 0 0 0-1.42l-2.34-2.34a1.003 1.003 0 0 0-1.42 0l-1.83 1.83 3.75 3.75 1.84-1.82z"/>
+					</svg>
+				</div>
                 <div>
                     <span class="pill pill-genero">${genero}</span>
                 </div>
