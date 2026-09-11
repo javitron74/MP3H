004793a8cfe9c46d4176ba82d465a8f39d4ebaed
javitron74 <josejaviertroncoso@gmail.com>
2026-09-11 13:04:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -209,6 +209,10 @@ function render() {
 				<span class="score-source"> 
 					${fuente !== "" ? `(${fuente})`: ""}
 				</span>
+				<!-- 🔥 BOTÓN EDITAR -->
+				<a href="form-edit.html?pos=${encodeURIComponent(item.Pos)}" class="btn-edit">
+					Editar
+				</a>
             </div>
         `;
 
