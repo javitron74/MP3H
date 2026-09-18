b2e9e11dab465a563687ec56de200819798a58ad
javitron74 <josejaviertroncoso@gmail.com>
2026-09-18 08:38:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -104,7 +104,7 @@ function resetWorker(){
 // Cargar datos desde Cloudflare KV
 async function cargarDatos() {
     try {
-        const res = await fetch(WORKER + "/mp3h.json");
+        const res = await fetch(WORKER + "/mp3h.json", headers: { "X-API-Key": "TU_TOKEN_SECRETO" });
         const data = await res.json();
 
         datos = Array.isArray(data) ? data : [];
