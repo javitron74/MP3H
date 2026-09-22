f6a15da7759f135136301d859522627ceb99f32d
javitron74 <josejaviertroncoso@gmail.com>
2026-09-22 07:42:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -249,13 +249,13 @@ function generarTabla(filtrados) {
             <td>
 				<div class="acciones">
 					<!-- Editar -->
-					<div class="edit-btn" onclick="location.href='form-edit.html?pos=${item.Pos}'">
+					<div onclick="location.href='form-edit.html?pos=${item.Pos}'">
 						<svg viewBox="0 0 24 24">
 							<path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04a1.003 1.003 0 0 0 0-1.42l-2.34-2.34a1.003 1.003 0 0 0-1.42 0l-1.83 1.83 3.75 3.75 1.84-1.82z"/>
 						</svg>
 					</div>
 					<!-- Eliminar -->
-					<div class="delete-btn" onclick="eliminar('${item.Pos}')">
+					<div onclick="eliminar('${item.Pos}')">
 						<svg viewBox="0 0 24 24">
 							<path d="M6 7h12l-1 12H7L6 7zm5-3h2l1 1h5v2H4V5h5l1-1z"/>
 						</svg>
