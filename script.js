aa295ba102fbb8d3a4f7e3ef5d9f026b1f3597cc
javitron74 <josejaviertroncoso@gmail.com>
2026-09-22 09:04:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -220,7 +220,11 @@ function generarTabla(filtrados) {
                 <th>Género</th>
                 <th>Estado</th>
                 <th>Emisión</th>
-                <th>Punt</th>
+                <th class="puntuacion-col">            
+					<svg class="estrella-head" viewBox="0 0 24 24">
+						<path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/>
+					</svg>
+				</th>
                 <th></th>
                 <th>Comentarios</th>
 				<th></th>
