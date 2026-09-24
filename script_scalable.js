faed41430b02bca30f67e787ca46f20862b471c5
javitron74 <josejaviertroncoso@gmail.com>
2026-09-24 16:14:16 +0200

    Update script_scalable.js

--- script_scalable.js
+++ script_scalable.js
@@ -463,7 +463,7 @@ function generarTablaNuevos(filtrados) {
             <td>
 				<div class="acciones">
 					<!-- Editar -->
-					<div onclick="location.href='form-edit.html?pos=${item.Pos}'">
+					<div onclick="location.href='form-nuevosgrupos.html?pos=${item.Pos}'">
 						<svg viewBox="0 0 24 24">
 							<path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04a1.003 1.003 0 0 0 0-1.42l-2.34-2.34a1.003 1.003 0 0 0-1.42 0l-1.83 1.83 3.75 3.75 1.84-1.82z"/>
 						</svg>
